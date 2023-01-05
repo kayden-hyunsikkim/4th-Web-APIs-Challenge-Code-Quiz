2bea56864ca82d314fb6f2073653b515dc33df9a
hyunsik kim (kayden) <kayden190116@gamil.com>
2023-01-06 00:41:01 +1030

    deleted wrong htmls and fixed js

--- assets/Quiz1.js
+++ assets/Quiz1.js
@@ -1,30 +1,54 @@
 let remainTime = document.querySelector("#timer");
 let startBtn = document.querySelector("#start");
-let timeLeft = 75;
+let main = document.querySelector("#Question");
 
 
-let timeInterval = setInterval(function () {
-    remainTime.innerHTML = "remain time : " + timeLeft;
-    timeLeft--;
-    if (timeLeft === -1) {
-        clearInterval(timeInterval);
-    }
-
-    console.log(timeLeft);
-
-}, 1000);
 
 
-function remainSecond() {
-    let remainsecond = localStorage.setItem("remain", timeLeft);
+function timer() {
+    let timeLeft = 75;
+    timeInterval = setInterval(() => {
+        remainTime.innerHTML = "remain time : " + timeLeft;
+        timeLeft--;
+        if (timeLeft === -1) {
+            clearInterval(timeInterval);
+        }
+    }, 1000)
 }
 
-function Correct() {
-    document.getElementById("Correct").style.display = "inline-block";
-    document.getElementById("next").style.display = "inline-block";
+function question1(event) {
+    event.preventDefault();
+
+    main.children[1].textContent = "JavaScript _______ refers to the process whereby the interpret appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of ths code.";
+
+    console.log(main.children[2])
+    main.children[2].remove();
+
+    var div = document.createElement("div");
+    div.setAttribute('id','mutiple-choice-box')
+    main.appendChild(div);
+    var button1 = document.createElement("button");
+    button1.textContent = "hi";
+    div.appendChild(button1);
+    button1.setAttribute('class','choices')
+    var button2 = document.createElement("button");
+    button2.textContent = "hello";
+    div.appendChild(button2);
+    button2.setAttribute('class','choices')
+    var button3 = document.createElement("button");
+    button3.textContent = "tell";
+    div.appendChild(button3);
+    button3.setAttribute('class','choices')
+    var button4 = document.createElement("button");
+    button4.textContent = "tell";
+    div.appendChild(button4);
+    button4.setAttribute('class','choices')
 }
 
-function Wrong() {
-    document.getElementById("Wrong").style.display = "inline-block";
-    document.getElementById("next").style.display = "inline-block";
-}
+
+
+
+
+
+startBtn.addEventListener("click", timer);
+startBtn.addEventListener("click", question1);
