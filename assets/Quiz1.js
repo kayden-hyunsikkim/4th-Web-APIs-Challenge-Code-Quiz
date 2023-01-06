a593e0898a4dc294d3f44da80e574f44bd8e2d56
hyunsik kim (kayden) <kayden190116@gamil.com>
2023-01-06 21:30:59 +1030

    added render choices and q2 choices

--- assets/Quiz1.js
+++ assets/Quiz1.js
@@ -1,7 +1,19 @@
 let remainTime = document.querySelector("#timer");
 let startBtn = document.querySelector("#start");
 let main = document.querySelector("#Question");
+let choiceBtns = document.querySelector("#choiceBtns");
 
+
+let count = 0;
+
+
+let questions = ["1. JavaScript _______ refers to the process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of ths code. Which of the following will fill in the blank?",
+
+    "2. Which of the following is not a keyword in a variable declaration?",
+
+    "3. The ___________ method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur. For example, this can be useful when: Clicking on a Submit button, prevent it from submitting a form"]
+
+let choices = {Q1 : ['Hammering','Hoisting','Cutting','Pasting'],   Q2 :['let','var','get','const'], Q3:['preventDefault()','blockDefault()','stopDefault()','dropDefault()']}
 
 
 
@@ -16,39 +28,49 @@ function timer() {
     }, 1000)
 }
 
-function question1(event) {
-    event.preventDefault();
 
-    main.children[1].textContent = "JavaScript _______ refers to the process whereby the interpret appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of ths code.";
+function question() {
+    let i = count;
+    main.children[1].textContent = questions[i];
+    startBtn.remove();
+    count++;
+}
 
-    console.log(main.children[2])
-    main.children[2].remove();
 
-    var div = document.createElement("div");
-    div.setAttribute('id','mutiple-choice-box')
-    main.appendChild(div);
-    var button1 = document.createElement("button");
-    button1.textContent = "hi";
-    div.appendChild(button1);
-    button1.setAttribute('class','choices')
-    var button2 = document.createElement("button");
-    button2.textContent = "hello";
-    div.appendChild(button2);
-    button2.setAttribute('class','choices')
-    var button3 = document.createElement("button");
-    button3.textContent = "tell";
-    div.appendChild(button3);
-    button3.setAttribute('class','choices')
-    var button4 = document.createElement("button");
-    button4.textContent = "tell";
-    div.appendChild(button4);
-    button4.setAttribute('class','choices')
+function renderChoice() {
+    for(i=0; i<choices.Q1.length; i++) {
+    let button = document.createElement("button");
+    button.setAttribute('class', 'choices');
+    button.textContent = choices.Q1[i];
+    choiceBtns.appendChild(button); 
+    }
+
+}
+
+function Q2Choicesorder() {
+    for(i=0; i < choices.Q2.length; i++){ 
+    let button = document.querySelector("#choiceBtns");
+    button.children[i].textContent = choices.Q2[i];  
+    }
+}
+
+function Q3Choicesorder() {
+    for(i=0; i < choices.Q3.length; i++){ 
+    let button = document.querySelector("#choiceBtns");
+    button.children[i].textContent = choices.Q3[i];  
+    }
 }
 
 
 
 
-
-
 startBtn.addEventListener("click", timer);
-startBtn.addEventListener("click", question1);
+startBtn.addEventListener("click", question);
+startBtn.addEventListener("click", renderChoice);
+
+choiceBtns.addEventListener("click", question);
+choiceBtns.addEventListener("click", Q2Choicesorder);
+
+
+
+
